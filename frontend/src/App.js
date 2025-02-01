fe70587a4def583f5a61be88d01d415601b7d6dd
p1roy <plroy0030@gmail.com>
2025-02-01 11:09:55 +0600

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Добро пожаловать в ROY!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
